1a0dd5f9f325e81e07a3fe89c3b743363e0062f3
Everton Silveira Benjamim <everton2610@gmail.com>
2024-09-10 21:45:18 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,109 @@
+let dados = [
+        {
+        titulo: "O Senhor dos Anéis - A sociedade do Anel",
+        genero: "Fantasia",
+        tags: "ian viggo vigo elijah",        
+        },
+        {
+            titulo: "Corra!",
+            genero: "Terror",
+            tags: "jordan peele racismo thriller"
+        },
+        {
+            titulo: "A Bruxa",
+            genero: "Terror",
+            tags: "século 17 bruxa nova inglaterra"
+        },
+        {
+            titulo: "Annihilation",
+            genero: "Ficção Científica",
+            tags: "alienígena mistério sci-fi"
+        },
+        {
+            titulo: "A Terra das Sombras",
+            genero: "Fantasia",
+            tags: "fantasia adolescentes amizade"
+        },
+        {
+            titulo: "O Farol",
+            genero: "Terror",
+            tags: "isolamento psicológico náufragos"
+        },
+        {
+            titulo: "A Chegada",
+            genero: "Ficção Científica",
+            tags: "alienígenas comunicação linguística"
+        },
+        {
+            titulo: "Duna",
+            genero: "Ficção Científica",
+            tags: "futurista espaço epic"
+        },
+        {
+            titulo: "O Bebê de Rosemary",
+            genero: "Terror",
+            tags: "demoníaco cult suspense"
+        },
+
+        {
+            titulo: "O Labirinto do Fauno",
+            genero: "Fantasia",
+            tags: "contos de fadas guerra civil"
+        },
+        {
+            titulo: "Midsommar",
+            genero: "Terror",
+            tags: "culto verão psicológico"
+        },
+        {
+            titulo: "Soul",
+            genero: "Fantasia",
+            tags: "alma música auto-descoberta"
+        },
+        {
+            titulo: "Blade Runner 2049",
+            genero: "Ficção Científica",
+            tags: "ciborgues distopia futurista"
+        },
+        {
+            titulo: "Tenet",
+            genero: "Ficção Científica",
+            tags: "viagem no tempo ação thriller"
+        },
+        {
+            titulo: "A Maldição da Residência Hill",
+            genero: "Terror",
+            tags: "família fantasmas mistério"
+        },
+        {
+            titulo: "O Hospedeiro",
+            genero: "Ficção Científica",
+            tags: "monstro rio família"
+        },
+        {
+            titulo: "O Ritmo da Morte",
+            genero: "Terror",
+            tags: "zumbis suspense sobrevivência"
+        },
+        {
+            titulo: "A Noite Passada",
+            genero: "Fantasia",
+            tags : "música magia escola"
+        },
+        {
+            titulo : "O Homem do Norte",
+            genero: "Fantasia",
+            tags: "viking vingança épico"
+        },
+        {
+            titulo: "Aquaman",
+            genero: "Fantasia",
+            tags: "super-herói atlântida aventura"
+        },
+        {
+            titulo: "O Mistério do Relógio na Parede",
+            genero: "Fantasia",
+            tags: "magia mistério aventura"
+        },
+    ]
+    
